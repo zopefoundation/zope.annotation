acbdf06d02e73cceafea38c29383f8ed42a08aff
Jim Fulton <jim@zope.com>
2004-03-21 11:35:15 +0000

    Added an import so that we can import IAttributeAnnotations from zope.app.annotation, makeing for a slightly shorter import.

--- __init__.py
+++ __init__.py
@@ -1,1 +1,19 @@
-# Import this.
+##############################################################################
+#
+# Copyright (c) 2004 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Annotation (meta data) support
+
+$Id: __init__.py,v 1.2 2004/03/21 11:35:15 jim Exp $
+"""
+
+from zope.app.annotation.interfaces import IAttributeAnnotatable
