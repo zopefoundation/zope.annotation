2cb8913bc2a63c52760e3b9ce10a2da8a9816da9
Alexander Heavner <alex@zope.com>
2007-02-12 21:36:31 +0000

    Added setup.py in order to 'eggify' the package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="annotation",
+    version="0.1dev",
+    include_package_data=True,
+    install_requirements = ['setuptools'],
+    zip_safe = False
+    )
