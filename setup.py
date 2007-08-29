cde3f9e05bfaa1cefd7323bf6d9a2c75a3039cfa
Baiju Muthukadan <baiju.m.mail@gmail.com>
2007-08-29 09:42:56 +0000

    Prepare for 3.4.0 release

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 ##############################################################################
 #
-# Copyright (c) 2006 Zope Corporation and Contributors.
+# Copyright (c) 2006-2007 Zope Corporation and Contributors.
 # All Rights Reserved.
 #
 # This software is subject to the provisions of the Zope Public License,
@@ -20,23 +20,51 @@ import os
 
 from setuptools import setup, find_packages
 
-setup(name='zope.annotation',
-      version='3.4.0b1',
-      url='http://svn.zope.org/zope.annotation',
-      license='ZPL 2.1',
-      description='Zope annotation',
-      author='Zope Corporation and Contributors',
-      author_email='zope3-dev@zope.org',
-      long_description="Annotations store arbitrary application"
-                       "data under package-unique keys.",
-      packages=find_packages('src'),
-      package_dir = {'': 'src'},
-      namespace_packages=['zope',],
-      install_requires=['setuptools',
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description = (
+    read('README.txt')
+    + '\n' +
+    read('CHANGES.txt')
+    + '\n' +
+    'Detailed Documentation\n'
+    '**********************\n'
+    + '\n' +
+    read('src', 'zope', 'annotation', 'README.txt') 
+    )
+
+setup(
+    name = 'zope.annotation',
+    version = '3.4.0',
+    url = 'http://pypi.python.org/pypi/zope.annotation',
+    license = 'ZPL 2.1',
+    description = 'Zope annotation',
+    author = 'Zope Corporation and Contributors',
+    author_email = 'zope3-dev@zope.org',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development',
+        ],
+    long_description = long_description,
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
+    namespace_packages = ['zope',],
+    install_requires = ['setuptools',
                         'zope.interface',
                         'zope.component',
                         'zope.location>=3.4.0b1.dev-r78903',
                         ],
-      include_package_data = True,
-      zip_safe = False,
-      )
+    extras_require = dict(
+        test = ['zope.testing',
+                'ZODB3'],
+        ),
+    include_package_data = True,
+    zip_safe = False,
+    )
