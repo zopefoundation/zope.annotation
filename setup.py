da627c83764e1f2c4d41545b4719b7e3ade7f3f7
Baiju Muthukadan <baiju.m.mail@gmail.com>
2009-04-01 21:13:34 +0000

    Whitespace fixes

--- setup.py
+++ setup.py
@@ -23,14 +23,14 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(
-    name = 'zope.annotation',
-    version = '3.5.0dev',
-    url = 'http://pypi.python.org/pypi/zope.annotation',
-    license = 'ZPL 2.1',
-    description = 'Object annotation mechanism',
-    author = 'Zope Corporation and Contributors',
-    author_email = 'zope-dev@zope.org',
-    classifiers = [
+    name='zope.annotation',
+    version='3.5.0dev',
+    url='http://pypi.python.org/pypi/zope.annotation',
+    license='ZPL 2.1',
+    description='Object annotation mechanism',
+    author='Zope Corporation and Contributors',
+    author_email='zope-dev@zope.org',
+    classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Zope Public License',
@@ -40,23 +40,23 @@ setup(
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Software Development',
         ],
-    long_description = \
+    long_description= \
         read('src', 'zope', 'annotation', 'README.txt') 
         + '\n\n' +
         read('CHANGES.txt'),
-    packages = find_packages('src'),
-    package_dir = {'': 'src'},
-    namespace_packages = ['zope',],
-    install_requires = ['setuptools',
-                        'zope.interface',
-                        'zope.component',
-                        'zope.location',
-                        'zope.proxy',
-                        ],
-    extras_require = dict(
-        test = ['zope.testing',
-                'ZODB3'],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['zope',],
+    install_requires=['setuptools',
+                      'zope.interface',
+                      'zope.component',
+                      'zope.location',
+                      'zope.proxy',
+                      ],
+    extras_require=dict(
+        test=['zope.testing',
+              'ZODB3'],
         ),
-    include_package_data = True,
-    zip_safe = False,
+    include_package_data=True,
+    zip_safe=False,
     )
