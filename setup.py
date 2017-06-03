c71e846748052d3d8e9f41b1983a3567fa995b68
Michael Howitz <mh@gocept.com>
2017-06-03 10:19:03 +0200

    Back to development: 4.6

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ tests_require = [
 
 setup(
     name='zope.annotation',
-    version='4.5',
+    version='4.6.dev0',
     url='https://github.com/zopefoundation/zope.annotation',
     license='ZPL 2.1',
     description='Object annotation mechanism',
