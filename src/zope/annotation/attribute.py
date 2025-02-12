6ff9a9d539f741a5c5597c7cc12b3bb2335c6ebb
Michael Howitz <icemac@gmx.net>
2025-02-12 15:56:09 +0100

    Update Python version support.

--- src/zope/annotation/attribute.py
+++ src/zope/annotation/attribute.py
@@ -18,7 +18,7 @@ from collections.abc import MutableMapping as DictMixin
 
 try:
     from BTrees.OOBTree import OOBTree as _STORAGE
-except ImportError:  # pragma: no cover
+except ModuleNotFoundError:  # pragma: no cover
     logging.getLogger(__name__).warning(
         'BTrees not available: falling back to dict for attribute storage')
     _STORAGE = dict
